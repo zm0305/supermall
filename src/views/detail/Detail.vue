2ca6a59884367ffff9524d17b7d87b808047aa09
ZM <415741492@qq.com>
2021-01-26 22:55:49 +0800

    详情页轮播图

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,25 +1,35 @@
 <template>
   <div id="detail">
     <detail-nav-bar></detail-nav-bar>
+    <detail-swiper :top-images="topImages"></detail-swiper>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navBar/NavBar";
 import DetailNavBar from "./childComps/DetailNavBar";
+import DetailSwiper from "./childComps/DetailSwiper";
+import { getDetail } from "network/detail";
+
 export default {
   name: "Detail",
   components: {
     NavBar,
     DetailNavBar,
+    DetailSwiper,
   },
   data() {
     return {
       iid: null,
+      topImages: [],
     };
   },
-  activated() {
+  created() {
     this.iid = this.$route.params.iid;
+    getDetail(this.iid).then((res) => {
+      this.topImages = res.result.itemInfo.topImages;
+      console.log(res);
+    });
   },
 };
 </script>
